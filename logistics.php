1d353de709118bcbfc746af6ba48ebaa3f72b056
klhribal <klhribal@iu.edu>
2024-02-10 17:31:36 -0500

    Adding json results

--- logistics.php
+++ logistics.php
@@ -108,7 +108,7 @@ const dataa = {
   labels: labels,
   datasets: [{
     label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
+    data: <?php echo $jsonResult2 ?>
     fill: false,
     borderColor: 'rgb(75, 192, 192)',
     tension: 0.1
