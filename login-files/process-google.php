339886a681f7485c6926dbdcdd18cc951fbbc227
klhribal <klhribal@iu.edu>
2024-02-12 21:31:32 -0500

    changes to processing

--- login-files/process-google.php
+++ login-files/process-google.php
@@ -1,9 +1,13 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Processing</title>
 
     <!--  library jwt-decode to handle JWT decoding -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jwt-decode/3.1.2/jwt-decode.min.js"></script>
