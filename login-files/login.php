60f2cb59ea52713c97c393784eec32fbdc32075c
klhribal <klhribal@iu.edu>
2024-02-12 21:41:01 -0500

    small tweak

--- login-files/login.php
+++ login-files/login.php
@@ -45,7 +45,7 @@
     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     const jsonPayload = decodeURIComponent(atob(base64).split('').map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join(''));
     const payload = JSON.parse(jsonPayload);
-    jQuery.ajax({
+    $.ajax({
         type: "POST",
         url: "process-google.php", // process user info + put into SQL
         data: {
