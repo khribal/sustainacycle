c3c91db3107e2c30237415e36d9e022e03c1b0c0
klhribal <klhribal@iu.edu>
2024-02-12 20:54:02 -0500

    process data

--- login-files/login.php
+++ login-files/login.php
@@ -31,7 +31,7 @@
 
 
     <script>
-function decodeJwtResponse(encodedToken) {
+    function decodeJwtResponse(encodedToken) {
     const base64Url = encodedToken.split('.')[1];
     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     const jsonPayload = decodeURIComponent(atob(base64).split('').map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join(''));
--- login-files/process-google.php
+++ login-files/process-google.php
@@ -11,14 +11,21 @@
 </head>
 <body>
     <?php
-
-// Check if the POST data is set
 if (isset($_POST['id_token'])) {
     // Get the ID token from the POST data
     $id_token = $_POST['id_token'];
 
-    // Echo the ID token to check if data is received
-    echo "Received ID Token: " . $id_token;
+    // Decode the ID token to get user information
+    $decoded_token = jwt_decode($id_token);
+
+    // Extract the desired user information
+    $user_name = $decoded_token->name;
+    $user_email = $decoded_token->email;
+
+    // Now you can use $user_name and $user_email as needed
+    // For example, you can store them in a database or echo them
+    echo "User Name: " . $user_name . "<br>";
+    echo "User Email: " . $user_email;
 } else {
     // Handle the case where the POST data is not set
     echo "Error: POST data not received.";
