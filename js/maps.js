8dc551a78837f19dde83a5b2360ce5715c3ded5f
klhribal <klhribal@iu.edu>
2024-02-16 14:21:46 -0500

    test

--- js/maps.js
+++ js/maps.js
@@ -7,7 +7,9 @@ async function initMap() {
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
+  
   // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
     zoom: 15,
