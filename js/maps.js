26b14e7ef58bc75d59615616b477b159078cde14
klhribal <klhribal@iu.edu>
2024-02-17 17:49:20 -0500

    Testing marker

--- js/maps.js
+++ js/maps.js
@@ -14,36 +14,44 @@ async function initMap() {
     mapId: "DEMO_MAP_ID",
   });
 
+  const marker = new google.maps.Marker({
+    position: position,
+    map: map,
+    title: 'Marker Title'
+  });
+  
   // Create a PlacesService instance
   const service = new google.maps.places.PlacesService(map);
 
   // Perform a text search
-  service.textSearch(
-    {
-      query: "recycling center",
-      // You can add more parameters if needed
-    },
-    (results, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (let place of results) {
-            // The markers (REMOVED CONST FROM LINE 30)
-            marker = new google.maps.Marker({
-                //test new google.maps.latlng
-                position: new google.maps.LatLng(place.geometry.location),
-                map: map,
-                title: place.name,
-            });
 
-            marker.setVisible(true);
-            marker.setMap(map);
 
-          console.log("Marker created: " + place.geometry.location)
+//   service.textSearch(
+//     {
+//       query: "recycling center",
+//     },
+//     (results, status) => {
+//       if (status === google.maps.places.PlacesServiceStatus.OK) {
+//         for (let place of results) {
+//             var marker = new google.maps.Marker({
+//                 position: new google.maps.LatLng(place.geometry.location),
+//                 map: map,
+//                 title: place.name,
+//             });
+
+//             marker.setVisible(true);
+//             marker.setMap(map);
+
+        //   console.log("Marker created: " + place.geometry.location)
+
+
+
           // Add a click event listener to the marker
         //   addMarkerClickListener(marker, place);
-        }
-      }
-    }
-  );
+//         }
+//       }
+//     }
+//   );
 }
 
 window.addEventListener('load', initMap);
