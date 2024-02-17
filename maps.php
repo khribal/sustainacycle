d5da8dfb34cb56eb11dc895d5b077b588827e76e
klhribal <klhribal@iu.edu>
2024-02-17 14:45:35 -0500

    moving links around

--- maps.php
+++ maps.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="css/styles.css"> -->
 
      <!-- Google Maps API -->
-     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAcZcRcS3sF91dolcW5Ft5SWBztjbBZYlM&libraries=places&callback=initMap" async defer></script>
+     <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAcZcRcS3sF91dolcW5Ft5SWBztjbBZYlM&libraries=places&callback=initMap" async defer></script> -->
           
     <!-- JS -->
     <!-- <script src="js/maps.js"></script> -->
@@ -30,6 +30,12 @@
   });
 </script>
      -->
+
+
+
+<!-- Google Maps API -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAcZcRcS3sF91dolcW5Ft5SWBztjbBZYlM&libraries=places&callback=initMap" async defer></script>
+
 
 <script>
 // Initialize and add the map
