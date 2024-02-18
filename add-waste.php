24cd0473b4a5797ff183a4b2f8c0b81d2eb48d31
Delaney Hupke <dhupke@iu.edu>
2024-02-18 10:49:27 -0500

    adding correct navbar

--- add-waste.php
+++ add-waste.php
@@ -9,29 +9,7 @@
     <title>See Available Waste</title>
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="team.php">About Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="project.php">About the Project</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="video.php">Promotional Video</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+<?php include('includes/nav.php'); ?>
 
 
   <h2>Add Waste</h2>
