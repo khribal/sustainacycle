60413ce77281627f9c629f7c7ddf51f93eb78e6d
Delaney Hupke <dhupke@iu.edu>
2024-02-17 22:25:19 -0500

    adding dropdown functionality to materials input

--- add-waste.php
+++ add-waste.php
@@ -33,13 +33,6 @@
   </div>
 </nav>
 
-<?php
-  $conn = mysqli_connect("db.luddy.indiana.edu", "i494f23_team20", "my+sql=i494f23_team20", "i494f23_team20");
-  if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
-
-?>
 
   <h2>Add Waste</h2>
   <form action="process-waste.php" method="POST">
@@ -47,7 +40,25 @@
     <input type="text" id="manufacturerID" name="manufacturerID"><br><br>
 
     <label for="materialName">Material Name:</label><br>
-    <input><br><br>
+    <select id="materialName" name="materialName">
+      <?php
+        $conn = mysqli_connect("db.luddy.indiana.edu", "i494f23_team20", "my+sql=i494f23_team20", "i494f23_team20");
+        if (!$conn) {
+          die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "SELECT DISTINCT materialName FROM materials";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()) {
+            echo "<option value=\"" . $row["materialName"] . "\">" . $row["materialName"] . "</option>";
+          }
+        }
+        $conn->close();
+
+        ?>
+    </select><br><br>
 
     <label for="quantity">Quantity:</label><br>
     <input type="number" id="quantity" name="quantity"><br><br>
