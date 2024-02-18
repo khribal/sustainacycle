04e9b31a7b2e3776a959cb92b907e6310cd25f14
Delaney Hupke <dhupke@iu.edu>
2024-02-17 22:32:58 -0500

    connecting process-waste.php to the database

--- process-waste.php
+++ process-waste.php
@@ -1,3 +1,17 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $manufacturerID = $_POST["manufacturerID"];
+    $materialName = $_POST["materialName"];
+    $quantity = $_POST["quantity"];
+
+    if (empty($manufacturerID) || empty($materialName) || empty($quantity)) {
+        echo "All fields must be filled.";
+    } else {
+        $conn = mysqli_connect("db.luddy.indiana.edu", "i494f23_team20", "my+sql=i494f23_team20", "i494f23_team20");
+        if (!$conn) {
+          die("Connection failed: " . mysqli_connect_error());
+        }
+    }
+}
 
 ?>
