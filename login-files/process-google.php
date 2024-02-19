0a031a47310d69e476e09d68b4818a4893027b04
klhribal <klhribal@iu.edu>
2024-02-19 12:42:53 -0500

    test

--- login-files/process-google.php
+++ login-files/process-google.php
@@ -3,6 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Get the raw POST data 
     $content = trim(file_get_contents("php://input")); 
     $decoded = json_decode($content, true); 
+    
     // Check if decoding was successful 
     if(is_array($decoded)) { 
         $id_token = $decoded['id_token']; 
@@ -13,7 +14,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo json_encode(['success' => true]); 
     } else { 
         echo json_encode(['error' => 'Invalid JSON.']); 
-    } } else { 
+    }} 
+else { 
         // Handle the case where the request method is not POST 
-        echo json_encode(['error' => 'POST data not received.']); }
+        echo json_encode(['error' => 'POST data not received.']); 
+}
+
+$json_error = json_last_error(); if ($json_error != JSON_ERROR_NONE) { echo json_encode(['error' => 'JSON decode error: ' . $json_error]); }
 ?>
