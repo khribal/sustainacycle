c5a66e24ea9dc0f70cee7508a1696e72b054055b
lsgulsen <lsgulsen@iu.edu>
2024-02-19 14:42:11 -0500

    Add-waste.php styling

--- add-waste.php
+++ add-waste.php
@@ -5,25 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="styles.css">
     <title>Add Waste</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
 </head>
 <body>
 <?php include('includes/nav.php'); ?>
 
 <div class="container px-4 mx-auto p-2">
-  <h1 class="video">Add Waste</h1>
-  <p class="video">Enter the information below to add available waste to our site.</p>
+  <h1 class="add">Add Waste</h1>
+  <p class="add">Enter the information below to add available waste to our site.</p>
 
 
-  <h2>Add Waste</h2>
+  <h2 class="add">Add Waste</h2>
   <form id="wasteForm" action="process-waste.php" method="POST">
-    <label for="manufacturerID">Manufacturer ID:</label><br>
+    <label for="manufacturerID" class="add">Manufacturer ID:</label><br>
     <input type="text" id="manufacturerID" name="manufacturerID"><br><br>
 
-    <label for="materialName">Material Name:</label><br>
+    <label for="materialName" class="add">Material Name:</label><br>
     <select id="materialName" name="materialName">  
-      <option value="">Select Material</option>
+      <option value="" class="add">Select Material</option>
       <?php
         $conn = mysqli_connect("db.luddy.indiana.edu", "i494f23_team20", "my+sql=i494f23_team20", "i494f23_team20");
         if (!$conn) {
@@ -43,10 +47,10 @@
         ?>
     </select><br><br>
 
-    <label for="quantity">Quantity:</label><br>
+    <label for="quantity" class="add">Quantity:</label><br>
     <input type="number" id="quantity" name="quantity"><br><br>
 
-    <label for="description">Description:</label>
+    <label for="description" class="add">Description:</label><br>
     <textarea id="description" name="description" rows="4" cols="50" readonly></textarea><br>
 
     <input type="submit" value="Submit">
