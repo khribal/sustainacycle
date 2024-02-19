9b879653ac095c5f340e102f3a844c0a4e28679c
klhribal <klhribal@iu.edu>
2024-02-19 13:39:02 -0500

    mod

--- login-files/config.php
+++ login-files/config.php
@@ -15,7 +15,7 @@ $google_client->setClientId('605347545950-imrjc8ufcpoeb1rv424p2ggd4qtghpku.apps.
 $google_client->setClientSecret('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');
 
 //Set the OAuth 2.0 Redirect URI
-$google_client->setRedirectUri('http://localhost/tutorial/php-login-using-google-demo/index.php');
+$google_client->setRedirectUri('https://cgi.luddy.indiana.edu/~team20/login-files/login-2.php');
 
 //
 $google_client->addScope('email');
