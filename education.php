a92f2ea1e0f8ab699b38e29ed616446b8d7a83d9
lsgulsen <lsgulsen@iu.edu>
2024-02-19 14:51:43 -0500

    Minor styling changes

--- education.php
+++ education.php
@@ -33,7 +33,7 @@ $result = mysqli_query($conn, $sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Educational Articles on Sustainability</title>
+    <title>Learn About Sustainability</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
