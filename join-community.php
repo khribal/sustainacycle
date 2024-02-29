f762b437cb9963707369a469cee4f6d9c90b3396
klhribal <klhribal@iu.edu>
2024-02-29 13:27:37 -0500

    Typo

--- join-community.php
+++ join-community.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include('./includes/boot-head.php')?>
 
-    <link rel="stylesheet" href=".css/styles.css">
+    <link rel="stylesheet" href="./css/styles.css">
     <title>Join A Community</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
